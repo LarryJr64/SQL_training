cc75817afb9b8727d9f277101d2739f59320b172
LarryJr64 <98753119+LarryJr64@users.noreply.github.com>
2023-02-21 17:53:02 +0100

    Add files via upload

---
+++ Hard/reponses_exercices_avancees.sql
@@ -0,0 +1,21 @@
+#1 Enregistrer le montant total de chaque commande dans le champ intitulé “cache_prix_total”
+
+#2 Obtenir le montant global de toutes les commandes, pour chaque mois
+
+#3 Obtenir la liste des 10 clients qui ont effectué le plus grand montant de commandes, et obtenir ce montant total pour chaque client.
+
+#4 Obtenir le montant total des commandes pour chaque date
+
+#5 Ajouter une colonne intitulée “category” à la table contenant les commandes. Cette colonne contiendra une valeur numérique
+
+#6 Enregistrer la valeur de la catégorie, en suivant les règles suivantes :
+# “1” pour les commandes de moins de 200€
+# “2” pour les commandes entre 200€ et 500€
+# “3” pour les commandes entre 500€ et 1.000€
+# “4” pour les commandes supérieures à 1.000€
+
+#7 Créer une table intitulée “commande_category” qui contiendra le descriptif de ces catégories
+
+#8 Insérer les 4 descriptifs de chaque catégorie au sein de la table précédemment créée
+
+#9 Supprimer toutes les commandes (et les lignes des commandes) inférieur au 1er février 2019. Cela doit être effectué en 2 requêtes maximum
